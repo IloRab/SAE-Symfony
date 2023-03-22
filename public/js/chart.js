cbaaaefd65857c33f8642ee76d25d100f7468f48
Oscar Lin <heartzephyr@gmail.com>
2023-03-22 11:06:54 +0100

    premier jet sur chart.js

---
+++ public/js/chart.js
@@ -0,0 +1,33 @@
+//récuperer les données json
+
+//Data Block
+const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+    label: '# of Votes',
+    data: [12, 19, 3, 5, 2, 3],
+    borderWidth: 1,
+    backgroundColor : 'yellow'
+    }]
+};
+
+//Config Block
+
+const config = {
+    data : data,
+    type: 'bar',
+        options: {
+        scales: {
+        y: {
+            beginAtZero: true
+        }
+        }
+    }
+};
+
+//Render Init Blocks
+
+const  myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+    );
